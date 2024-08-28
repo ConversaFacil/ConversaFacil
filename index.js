759a37f4fd6d391bafafd6bc768138184bd31440
roxootiago <tiago.roxo@hotmail.com>
2024-08-28 11:25:01 -0300

    fix: evitando enviar com form vazio

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 function SendMail(event) {
-  event.preventDefault(); // Evita o carregamento da página
+  event.preventDefault(); 
 
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let message = document.getElementById("message").value;
 
+  if (name.length === 0 || email.length === 0 || message.length === 0) {
+    alert("Por favor, preencha todos os campos antes de enviar.");
+    return; 
+  }
+
   let params = {
     from_name: name,
-    email_id    : email,
+    email_id: email,
     message: message,
   };
 
   emailjs
     .send("service_vgkpo3s", "template_b8yo8d9", params)
     .then(function (response) {
-      alert("Success! Status: " + response.status);
+      alert("Sucesso! Status: " + response.status);
     })
     .catch(function (error) {
-      alert("Failed to send email: " + error);
+      alert("Falha ao enviar o email: " + error);
     });
 }
-
