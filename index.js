d9bc4149751718893bb0d3205c3311954ea42186
roxootiago <tiago.roxo@hotmail.com>
2024-08-29 18:36:16 -0300

    feat: incluindo erro para preencher campo

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function SendMail(event) {
   event.preventDefault();
 
+  // Se a validação falhar, interrompe o envio do email
+  if (!validateForm()) {
+    return;
+  }
+
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let message = document.getElementById("message").value;
   let copy = document.getElementById("copy").checked;
 
-  if (name.length === 0 || email.length === 0 || message.length === 0) {
-    alert("Por favor, preencha todos os campos antes de enviar.");
-    return;
-  }
-
   let params = {
     from_name: name,
     email_id: email,
     message: message,
-    cc: copy ? email : undefined, 
+    cc: copy ? email : undefined,
   };
 
   emailjs
@@ -26,7 +26,7 @@ function SendMail(event) {
       document.getElementById("name").value = "";
       document.getElementById("email").value = "";
       document.getElementById("message").value = "";
-      document.getElementById("copy").checked = false; 
+      document.getElementById("copy").checked = false;
 
       history.replaceState(null, "", location.pathname);
       location.reload();
@@ -34,4 +34,45 @@ function SendMail(event) {
     .catch(function (error) {
       alert("Falha ao enviar o email: " + error);
     });
+}
+
+function validateForm() {
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+
+  const nameError = document.getElementById("nameError");
+  const emailError = document.getElementById("emailError");
+  const messageError = document.getElementById("messageError");
+
+  let valid = true;
+
+  if (nameInput.value === "") {
+    nameError.classList.remove("hidden");
+    nameInput.classList.add("border-red-600");
+    valid = false;
+  } else {
+    nameError.classList.add("hidden");
+    nameInput.classList.remove("border-red-600");
+  }
+
+  if (emailInput.value === "") {
+    emailError.classList.remove("hidden");
+    emailInput.classList.add("border-red-600");
+    valid = false;
+  } else {
+    emailError.classList.add("hidden");
+    emailInput.classList.remove("border-red-600");
+  }
+
+  if (messageInput.value === "") {
+    messageError.classList.remove("hidden");
+    messageInput.classList.add("border-red-600");
+    valid = false;
+  } else {
+    messageError.classList.add("hidden");
+    messageInput.classList.remove("border-red-600");
+  }
+
+  return valid;
 }
